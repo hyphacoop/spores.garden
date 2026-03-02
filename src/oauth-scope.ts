e672488561609ce99c468d2077a35147893a6d70
vincent <vincentcharlebois@gmail.com>
2026-03-02 16:08:26 -0500

    Replace transitional OAuth scope with granular permissions

---
+++ src/oauth-scope.ts
@@ -0,0 +1,13 @@
+export const ATPROTO_GRANULAR_SCOPE = [
+  'atproto',
+  'repo:coop.hypha.spores.site/*',
+  'repo:coop.hypha.spores.content/*',
+  'repo:coop.hypha.spores.social/*',
+  'repo:coop.hypha.spores.item/*',
+  'repo:garden.spores.site/*',
+  'repo:garden.spores.content/*',
+  'repo:garden.spores.social/*',
+  'repo:garden.spores.item/*',
+  'blob:coop.hypha.spores.*',
+  'blob:garden.spores.*',
+].join(' ');
